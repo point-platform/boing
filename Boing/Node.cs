e10f328b38d489d93234cd3d7fba66b0ba4d8890
Drew Noakes <git@drewnoakes.com>
2016-05-21 17:59:00 +0100

    Use Nullable<Vector2f> for optional parameter. Zero is a valid intial value.

--- Boing/Node.cs
+++ Boing/Node.cs
@@ -12,12 +12,12 @@ namespace Boing
         public Vector2f Position { get; set; }
         public Vector2f Velocity { get; private set; }
 
-        public Node(string id, float mass = 1.0f, float damping = 0.5f, Vector2f position = default(Vector2f))
+        public Node(string id, float mass = 1.0f, float damping = 0.5f, Vector2f? position = null)
         {
             Id = id;
             Mass = mass;
             Damping = damping;
-            Position = position != Vector2f.Zero ? position : Vector2f.Random();
+            Position = position ?? Vector2f.Random();
         }
 
         public void ApplyForce(Vector2f force)
