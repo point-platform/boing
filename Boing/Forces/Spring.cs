ca5144714bf1bdffe163c9ac15a9f4d9d4d836b8
Drew Noakes <git@drewnoakes.com>
2017-04-05 22:28:55 +0100

    Document Spring API

--- Boing/Forces/Spring.cs
+++ Boing/Forces/Spring.cs
@@ -22,15 +22,62 @@ using System.Diagnostics;
 namespace Boing
 {
     /// <summary>
-    /// Models a linear spring between two <see cref="PointMass"/> instances according to Hooke's law.
+    /// A force that models a linear spring between two point masses according to Hooke's law.
     /// </summary>
+    /// <remarks>
+    /// Unlike many other <see cref="IForce"/> implementations, a single <see cref="Spring"/>
+    /// instance does not apply to all point masses in the simulation. It applies to two, only.
+    /// <para />
+    /// The spring is defined by its two point masses, a length and a spring constant.
+    /// At each time step, the distance between the point masses is computed and compared
+    /// against the spring's length.
+    /// <para />
+    /// If the distance matches the length, no force is applied.
+    /// When the distance diverges from the spring's length, the spring undergoes compression
+    /// or elongation and exerts a restorative force against both point masses.
+    /// <para />
+    /// Note that if one point mass is pinned, the force applies doubly to the unpinned end.
+    /// If both point masses are pinned, the spring has no effect.
+    /// <para />
+    /// See https://en.wikipedia.org/wiki/Hooke%27s_law for more information.
+    /// </remarks>
     public sealed class Spring : IForce
     {
+        /// <summary>
+        /// Gets one of the two point masses connected by this spring.
+        /// </summary>
+        /// <remarks>
+        /// The orientation of the spring has no effect. <see cref="Source"/> and <see cref="Target"/>
+        /// could be swapped and the outcome would be identical.
+        /// </remarks>
         public PointMass Source { get; }
+
+        /// <summary>
+        /// Gets one of the two point masses connected by this spring.
+        /// </summary>
+        /// <remarks>
+        /// The orientation of the spring has no effect. <see cref="Source"/> and <see cref="Target"/>
+        /// could be swapped and the outcome would be identical.
+        /// </remarks>
         public PointMass Target { get; }
+
+        /// <summary>
+        /// Gets and sets the resting length of the spring.
+        /// </summary>
         public float Length { get; set; }
+
+        /// <summary>
+        /// Gets and sets the spring constant.
+        /// </summary>
         public float K { get; set; }
 
+        /// <summary>
+        /// Initialises a new instance of <see cref="Spring"/>.
+        /// </summary>
+        /// <param name="source">One of the two point masses.</param>
+        /// <param name="target">One of the two point masses.</param>
+        /// <param name="length">The resting length of the spring. The default value is 100 units.</param>
+        /// <param name="k">The string constant. The default value is 80.</param>
         public Spring(PointMass source, PointMass target, float length = 100.0f, float k = 80.0f)
         {
             Source = source;
@@ -39,8 +86,16 @@ namespace Boing
             K = k;
         }
 
+        /// <summary>
+        /// Gets a 2D line segment whose start and end points match those of
+        /// the <see cref="Source"/> and <see cref="Target"/> point mass positions.
+        /// </summary>
         public LineSegment2f LineSegment => new LineSegment2f(Source.Position, Target.Position);
 
+        /// <summary>
+        /// Gets the 2D axis-aligned bounding box that encloses the <see cref="Source"/> and
+        /// <see cref="Target"/> point mass positions.
+        /// </summary>
         public Rectangle2f Bounds => new Rectangle2f(
             Math.Min(Source.Position.X, Target.Position.X),
             Math.Min(Source.Position.Y, Target.Position.Y),
