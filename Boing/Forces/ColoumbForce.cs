b62428423975e403458ceb39fae625ae995afc1a
Drew Noakes <git@drewnoakes.com>
2021-01-06 22:24:31 +1100

    Bump copyright year

--- Boing/Forces/ColoumbForce.cs
+++ Boing/Forces/ColoumbForce.cs
@@ -1,6 +1,6 @@
 #region License
 
-//  Copyright 2015-2020 Drew Noakes, Krzysztof Dul
+//  Copyright 2015-2021 Drew Noakes, Krzysztof Dul
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
